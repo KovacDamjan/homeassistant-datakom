68e626a42c68f75c5de5352c8f1b69ed30661e6f
Damjan Kovač <4171751+KovacDamjan@users.noreply.github.com>
2026-07-14 06:33:57 +0200

    Add integration constants

---
+++ custom_components/datakom/const.py
@@ -0,0 +1,11 @@
+DOMAIN = "datakom"
+PLATFORMS = ["sensor", "binary_sensor"]
+
+CONF_UNIT_ID = "unit_id"
+CONF_SCAN_INTERVAL = "scan_interval"
+
+DEFAULT_PORT = 502
+DEFAULT_UNIT_ID = 1
+DEFAULT_SCAN_INTERVAL = 10
+
+MANUFACTURER = "Datakom"
